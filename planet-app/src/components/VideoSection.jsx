f9198f2f4680cb42c2c68940abc7256943aa4e4a
itzemmadox <eminence1973@gmail.com>
2026-05-20 13:19:52 +0100

    feat: add planet video to VideoSection, added asset folder for video folder and improve layout responsiveness

--- planet-app/src/components/VideoSection.jsx
+++ planet-app/src/components/VideoSection.jsx
@@ -6,19 +6,20 @@
 import React from 'react';
 import { motion } from 'motion/react';
 import { Play } from 'lucide-react';
+import planetVideo from '../assets/video/planet.mp4';
 
 export default function VideoSection() {
   return (
-    <section className="py-24! md:py-[70px]! lg:py-24! px-8! flex justify-center items-center bg-white text-black text-center relative">
-      <div className="max-w-5xl mx-auto">
+    <section className="overflow-hidden py-24! px-8! flex justify-center items-center bg-white text-black text-center relative">
+      <div className="max-w-5xl w-full mx-auto overflow-hidden">
         <motion.div
            initial={{ opacity: 0, y: 30 }}
            whileInView={{ opacity: 1, y: 0 }}
            viewport={{ once: true }}
            transition={{ duration: 0.8 }}
         >
-          <div className="flex flex-col md:flex-row-reverse lg:flex-col md:items-center md:justify-between gap-12 text-center md:text-left">
-            <div className="md:w-2/3 lg:w-full">
+          <div className="flex flex-col md:flex-row-reverse lg:flex-col md:items-center md:justify-between gap-12 text-center md:text-left w-full">
+            <div className="min-w-0 md:w-2/3 lg:w-full">
               <h2 className="text-2xl md:text-2xl lg:text-4xl text-center font-bold text-blue-900 mb-2 tracking-tight font-sans md:font-display">
                 How Planetary Data Helps Us Understand Space
               </h2>
@@ -27,8 +28,8 @@ export default function VideoSection() {
               </p>
             </div>
 
-            <div className="md:w-1/3 lg:w-full md:h-96 lg:h-auto md:flex lg:block items-center justify-center">
-              <div className="w-full h-[15rem] md:h-64 lg:h-full relative group rounded-2xl overflow-hidden aspect-video">
+            <div className="min-w-0 md:w-1/3 lg:w-full md:h-96 lg:h-auto md:flex lg:block items-center justify-center">
+              <div className="w-full max-w-full h-[15rem] md:h-64 lg:h-full relative group rounded-2xl overflow-hidden aspect-video">
                 
                 <video
                   autoPlay
@@ -38,7 +39,8 @@ export default function VideoSection() {
                   className="w-full h-full object-cover opacity-90 transition-transform duration-700 group-hover:scale-100"
                 >
                   <source
-                    src="https://www.pexels.com/download/video/36028182/"
+                    // src="https://www.pexels.com/download/video/36028182/"
+                    src={planetVideo}
                     type="video/mp4"
                   />
                 </video>
